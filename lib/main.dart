849f8b0787a9c71a3de89067beb54f8cb81522b9
Md. Abir Hasan <abirhasanshuvo789@gmail.com>
2025-08-30 15:12:26 +0600

    stateful provider is working

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_v3/video_1/video_1.dart';
 import 'package:riverpod_v3/video_2/video_2.dart';
+import 'package:riverpod_v3/video_3/video_3.dart';
 
 void main() {
   runApp(
     ProviderScope(
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: StateProviderTutorial(),
+        home: StateFulConsumerTutorial(),
       ),
     ),
   );
