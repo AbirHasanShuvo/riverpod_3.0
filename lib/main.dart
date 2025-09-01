c5d4ccdb179e0030793a3519323e99962467fe9b
Md. Abir Hasan <abirhasanshuvo789@gmail.com>
2025-09-01 09:41:11 +0600

    done with state notifier

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_v3/section_2/video_5/video_5.dart';
-import 'package:riverpod_v3/section_2/video_6/video_6.dart';
 import 'package:riverpod_v3/section_2/video_7/counter_example/counter_screen.dart';
+import 'package:riverpod_v3/section_2/video_7/todo_example/todo_screen.dart';
 
 void main() {
   runApp(
     ProviderScope(
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: CounterScreen(),
-      ),
+      child: MaterialApp(debugShowCheckedModeBanner: false, home: TodoScreen()),
     ),
   );
 }
